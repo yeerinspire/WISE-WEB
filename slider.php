8f3d80b9c4f15909a19b1d49ccaa8f57aeebead2
yeerinspire <56554256+yeerinspire@users.noreply.github.com>
2021-02-04 09:33:11 +0700

    Add files via upload

---
+++ slider.php
@@ -0,0 +1,80 @@
+<!-- ======= Intro Section ======= -->
+<div class="intro intro-carousel">
+    <div id="carousel" class="owl-carousel owl-theme">
+      <div class="carousel-item-a intro-item bg-image" style="background-image: url(assets/img/slide-1.jpg)">
+        <div class="overlay overlay-a"></div>
+        <div class="intro-content display-table">
+          <div class="table-cell">
+            <div class="container">
+              <div class="row">
+                <div class="col-lg-8">
+                  <div class="intro-body">
+                    <p class="intro-title-top">Pandaan, Indonesia
+                      <br> 78345
+                    </p>
+                    <h1 class="intro-title mb-4">
+                      <span class="color-b">Sunrise </span>Hills
+                      <br> Taman Dayu
+                    </h1>
+                    <p class="intro-subtitle intro-price">
+                      <a href="#"><span class="price-a">Jual | Rp. 450.000.000</span></a>
+                    </p>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+      <div class="carousel-item-a intro-item bg-image" style="background-image: url(assets/img/slide-2.jpg)">
+        <div class="overlay overlay-a"></div>
+        <div class="intro-content display-table">
+          <div class="table-cell">
+            <div class="container">
+              <div class="row">
+                <div class="col-lg-8">
+                  <div class="intro-body">
+                    <p class="intro-title-top">Sidoarjo, Jawa Timur
+                      <br> 78345
+                    </p>
+                    <h1 class="intro-title mb-4">
+                      <span class="color-b">Griya </span> Sakinah
+                      <br> Taman Palma
+                    </h1>
+                    <p class="intro-subtitle intro-price">
+                      <a href="#"><span class="price-a">Promo | Rp. 500.000.000</span></a>
+                    </p>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+      <div class="carousel-item-a intro-item bg-image" style="background-image: url(assets/img/slide-3.jpg)">
+        <div class="overlay overlay-a"></div>
+        <div class="intro-content display-table">
+          <div class="table-cell">
+            <div class="container">
+              <div class="row">
+                <div class="col-lg-8">
+                  <div class="intro-body">
+                    <p class="intro-title-top">Jakarta, Indonesia
+                      <br> 78345
+                    </p>
+                    <h1 class="intro-title mb-4">
+                      <span class="color-b">Pondok </span> Indah
+                      <br> Kebumen
+                    </h1>
+                    <p class="intro-subtitle intro-price">
+                      <a href="#"><span class="price-a">Disewakan | $ 10.000.000</span></a>
+                    </p>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  </div><!-- End Intro Section -->
